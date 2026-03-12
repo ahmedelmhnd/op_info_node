0ca3935276b78f3f5238b97f8c6f361cda2837b0
ahmed <ahmed.elmohanned@gmail.com>
2026-03-12 11:58:29 +0800

    refactored code to use express instead of plauing node

--- index.js
+++ index.js
@@ -1,37 +1,54 @@
-const http = require('node:http');
-const fs = require('node:fs');
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    let statusCode = 200;
+app.get("/", (req, res) => res.sendFile("/home/ahmed/repos/op_info_node/index.html"));
+app.get("/about", (req, res) => res.sendFile("/home/ahmed/repos/op_info_node/about.html"));
+app.get("/contact-me", (req, res) => res.sendFile("/home/ahmed/repos/op_info_node/contact-me.html"));
+app.get("*splat", (req, res) => res.sendFile("/home/ahmed/repos/op_info_node/404.html"));
 
-    let page = '';
-    console.log(req.url);
-
-    switch (req.url) {
-        case "/":
-            page = './index.html';
-            break;
-        case "/about":
-            page = './about.html';
-            break;
-        case "/contact-me":
-            page = './contact-me.html';
-            break;
-        default:
-            page = "./404.html"
-            statusCode = 404;
-            break;
+const PORT = 3000;
+app.listen(PORT, (error) => {
+    if (error) {
+        throw error;
     }
+})
 
-    fs.readFile(page, 'utf8', (err, data) => {
-        if (err) {
-        console.error(err);
-        return;
-        }
-    res.writeHead(statusCode, {'Content-Type' : 'text/html' });
-    res.end(data);
-    });
-});
 
-server.listen(8080);
+
+// const http = require('node:http');
+// const fs = require('node:fs');
+
+// const server = http.createServer((req, res) => {
+//     let statusCode = 200;
+
+//     let page = '';
+//     console.log(req.url);
+
+//     switch (req.url) {
+//         case "/":
+//             page = './index.html';
+//             break;
+//         case "/about":
+//             page = './about.html';
+//             break;
+//         case "/contact-me":
+//             page = './contact-me.html';
+//             break;
+//         default:
+//             page = "./404.html"
+//             statusCode = 404;
+//             break;
+//     }
+
+//     fs.readFile(page, 'utf8', (err, data) => {
+//         if (err) {
+//         console.error(err);
+//         return;
+//         }
+//     res.writeHead(statusCode, {'Content-Type' : 'text/html' });
+//     res.end(data);
+//     });
+// });
+
+// server.listen(8080);
 
